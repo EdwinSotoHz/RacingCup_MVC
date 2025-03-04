54b04929f4898b8a489fce171bd6c7a9266d36bc
Edwin Salvador Soto Hernandez <230110358@itsoeh.edu.mx>
2025-03-03 23:52:01 -0600

    Tabla nuevo diseño 1

--- RacingCup/RacingCup/obj/Debug/net8.0/RacingCup.AssemblyInfo.cs
+++ RacingCup/RacingCup/obj/Debug/net8.0/RacingCup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c18e3680539479f4da5a8bc2dbba920bd68e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbb9b24cf18cd3595c7048326c65bf8f569ff14")]
 [assembly: System.Reflection.AssemblyProductAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RacingCup/RacingCup/wwwroot/js/DrawLines.js
@@ -0,0 +1,43 @@
+﻿function openModal(row, col, category, teamCount) {
+    // Obtener los datos del equipo desde la variable global
+    var team = teamsData[row][col];
+
+    // Mostrar los datos en la ventana modal
+    document.getElementById("modalTeamName").innerText = team.name;
+    document.getElementById("modalTeamElements").innerText = team.elements;
+    document.getElementById("modalTeamSchool").innerText = team.school;
+    document.getElementById("modalTeamCategory").innerText = team.category;
+    document.getElementById("modalTeamBestTimeInput").value = team.bestTime;
+    document.getElementById("modalTeamStatus").innerText = team.status ? "Activo" : "Inactivo";
+
+    // Asignar valores a los campos ocultos
+    document.getElementById("modalRow").value = row;
+    document.getElementById("modalCol").value = col;
+    document.getElementById("modalCategory").value = category;
+
+    document.getElementById("modalRowLoser").value = row;
+    document.getElementById("modalColLoser").value = col;
+    document.getElementById("modalCategoryLoser").value = category;
+
+    // Mostrar u ocultar la opción "Desclasificar"
+    var formSetLoser = document.getElementById("formSetLoser");
+    if (teamCount % 2 !== 0 && col === teamCount - 1) {
+        formSetLoser.style.display = "inline-block"; // Mostrar si el equipo está solo
+    } else {
+        formSetLoser.style.display = "none"; // Ocultar si el equipo tiene pareja
+    }
+
+    // Mostrar la ventana modal
+    document.getElementById("teamModal").style.display = "block";
+}
+
+function closeModal() {
+    // Ocultar la ventana modal
+    document.getElementById("teamModal").style.display = "none";
+}
+
+// Función para actualizar el mejor tiempo antes de enviar el formulario
+function setBestTime(form) {
+    var bestTime = document.getElementById("modalTeamBestTimeInput").value;
+    form.querySelector("input[name='bestTime']").value = bestTime;
+}
