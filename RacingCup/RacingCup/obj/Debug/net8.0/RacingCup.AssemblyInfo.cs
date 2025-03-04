de1924b8596e0d71e29f944c7a68f12d5c5b4269
Edwin Salvador Soto Hernandez <230110358@itsoeh.edu.mx>
2025-03-04 00:14:36 -0600

    Tabla nuevo diseño 1

--- RacingCup/RacingCup/obj/Debug/net8.0/RacingCup.AssemblyInfo.cs
+++ RacingCup/RacingCup/obj/Debug/net8.0/RacingCup.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbb9b24cf18cd3595c7048326c65bf8f569ff14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b04929f4898b8a489fce171bd6c7a9266d36bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RacingCup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
